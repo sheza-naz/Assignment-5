da6099c460034bf7de5d7d2ebfbbca6405190d78
sheza-naz <64827065+sheza-naz@users.noreply.github.com>
2020-07-06 19:53:19 +0500

    done
    changing

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    public static int ScreenNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (ScreenNumber == 0)
+        {
+            StartCoroutine(toSplash());
+        }
+    }
+
+    // Update is called once per frame
+    IEnumerator toSplash()
+    {
+        yield return new WaitForSeconds(5);
+        ScreenNumber = 1;
+        SceneManager.LoadScene(ScreenNumber);
+
+    }
+}
